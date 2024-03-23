6daa6864c7133d1c4a6671fbb4ad36e13660d31a
David Watson <davidlwatsonjr@users.noreply.github.com>
2024-03-23 00:03:19 -0400

    Get basic movie search implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 import createTheme from "@mui/material/styles/createTheme";
 import "./App.css";
 import "@fontsource/roboto/300.css";
@@ -14,12 +17,44 @@ const darkTheme = createTheme({
   },
 });
 
+const MOVIES_API_URL = "https://movies.davidlwatsonjr.com/movies";
+
 function App() {
+  const [queryTerm, setQueryTerm] = useState("");
+  const [movieList, setMovieList] = useState({});
+
+  const handleQueryTermChange = (event) => {
+    setQueryTerm(event.target.value);
+  };
+
+  const handleSearch = async () => {
+    const url = `${MOVIES_API_URL}?query_term=${queryTerm}`;
+    const moviesResponse = await fetch(url);
+    const movies = await moviesResponse.json();
+    setMovieList(movies);
+  };
+
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline enableColorScheme />
       <Container className="App">
         <h1>Movie List</h1>
+        <TextField
+          size="small"
+          fullWidth
+          placeholder="Search movies..."
+          onChange={handleQueryTermChange}
+        />
+        <Button variant="contained" color="primary" onClick={handleSearch}>
+          Search
+        </Button>
+        <ul>
+          {movieList?.undownloadedAvailableMovies?.map((movie) => (
+            <li key={movie.url}>
+              {movie.title} ({movie.year})
+            </li>
+          ))}
+        </ul>
       </Container>
     </ThemeProvider>
   );
