df7958277e29ca1bf43654002009de92ddee6b1d
David Watson <davidlwatsonjr@users.noreply.github.com>
2024-03-22 23:41:40 -0400

    Get some basic theming and meta in place

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
-import logo from './logo.svg';
+import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
+import ThemeProvider from "@mui/material/styles/ThemeProvider";
+import createTheme from "@mui/material/styles/createTheme";
 import "./App.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 function App() {
   return (
-    <div className="App">
-      <CssBaseline />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline enableColorScheme />
+      <Container className="App">
+        <h1>Movie List</h1>
+      </Container>
+    </ThemeProvider>
   );
 }
 
