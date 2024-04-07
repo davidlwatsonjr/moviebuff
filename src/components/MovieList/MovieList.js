c2773be5b3448fe7bf3db4a70a9204cc25a947e4
David Watson <davidlwatsonjr@users.noreply.github.com>
2024-04-07 11:35:24 -0400

    Make movie Avatar clickable link

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -8,44 +8,59 @@ import ListItemAvatar from "@mui/material/ListItemAvatar";
 import ListItemText from "@mui/material/ListItemText";
 import DownloadLinks from "../DownloadLinks/DownloadLinks";
 
-function MovieList({ movies }) {
+function MovieList({ movies, showDownloaded }) {
   return (
     <List>
-      {movies?.map?.((movie) => (
-        <ListItem key={movie.url} disableGutters>
-          <ListItemAvatar>
-            <Avatar
-              alt={movie.title}
-              src={movie.medium_cover_image}
-              sx={{ marginRight: 2, width: 64, height: 64 }}
-            />
-          </ListItemAvatar>
-          <ListItemText
-            primary={
-              <>
-                <Link
-                  href={movie.url}
-                  underline="hover"
-                  target="_blank"
-                  rel="noreferrer"
-                  title={movie.date_uploaded}
-                >
-                  {movie.title}
-                </Link>{" "}
-                ({movie.year}-{movie.language}) ⭐ {movie.rating}
-              </>
-            }
-            secondary={<DownloadLinks links={movie.torrents} />}
-          ></ListItemText>
-          {movie.plexEquivalent && <CheckIcon color="success" />}
-        </ListItem>
-      ))}
+      {movies?.map?.((movie) => {
+        if (!showDownloaded && movie.plexEquivalent) {
+          return null;
+        }
+
+        return (
+          <ListItem key={movie.url} disableGutters>
+            <ListItemAvatar>
+              <Link
+                href={movie.url}
+                underline="hover"
+                target="_blank"
+                rel="noreferrer"
+                title={movie.title}
+              >
+                <Avatar
+                  alt={movie.title}
+                  src={movie.medium_cover_image}
+                  sx={{ marginRight: 2, width: 64, height: 64 }}
+                />
+              </Link>
+            </ListItemAvatar>
+            <ListItemText
+              primary={
+                <>
+                  <Link
+                    href={movie.url}
+                    underline="hover"
+                    target="_blank"
+                    rel="noreferrer"
+                    title={movie.date_uploaded}
+                  >
+                    {movie.title}
+                  </Link>{" "}
+                  ({movie.year}-{movie.language}) ⭐ {movie.rating}
+                </>
+              }
+              secondary={<DownloadLinks links={movie.torrents} />}
+            ></ListItemText>
+            {movie.plexEquivalent && <CheckIcon color="success" />}
+          </ListItem>
+        );
+      })}
     </List>
   );
 }
 
 MovieList.propTypes = {
   movies: PropTypes.arrayOf(Object),
+  showDownloaded: PropTypes.bool,
 };
 
 export default MovieList;
